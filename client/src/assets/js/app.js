f0afa4f573404340a98181b5681e37099e0f9f1c
Carlos Olmedo <carlos.olmedodev@gmail.com>
2019-10-20 17:52:17 +0200

    [+] fatboar CI v1

---
+++ client/src/assets/js/app.js
@@ -0,0 +1,10 @@
+// CSS
+ import 'bootstrap/dist/css/bootstrap.css';
+//import '../css/style.css';
+ 
+// JS
+import $ from 'jquery';
+//const $ = require('jquery');
+ 
+import 'bootstrap';
+//import '@fortawesome/fontawesome-free/js/all';
